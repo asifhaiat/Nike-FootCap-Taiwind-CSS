f6101777b601ed9ac7680b7dcebf5acce360cf72
asifhaiat <asifhaiat674652gmail.com>
2023-04-10 14:51:04 +0600

    header part completed

---
+++ js/app.js
@@ -0,0 +1,15 @@
+/* const navLinks = document.querySelectorAll("#user-interaction");
+console.log(navLinks);
+const hoveredLink = document.querySelector("#hovered-user");
+navLinks.forEach((link) => {
+  link.addEventListener("mouseover", () => {
+    const name = link.getAttribute("data-name");
+    if (name) {
+      hoveredLink.textContent = link.textContent;
+    }
+  });
+  link.addEventListener("mouseout", () => {
+    hoveredLink.textContent = " ";
+  });
+});
+ */
